cfd10c9abc94d699df2beef0627f46b38091c4c3
zhangguangxin1234 <zhangguangxin1234@126.com>
2024-08-18 13:45:34 +0800

    Update model.py

--- src/langgraph_engineer/model.py
+++ src/langgraph_engineer/model.py
@@ -5,7 +5,7 @@ from langchain_anthropic import ChatAnthropic
 def _get_model(config, default, key):
     model = config['configurable'].get(key, default)
     if model == "openai":
-        return ChatOpenAI(temperature=0, model_name="gpt-4o-2024-08-06")
+        return ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
     elif model == "anthropic":
         return ChatAnthropic(temperature=0, model_name="claude-3-5-sonnet-20240620")
     else:
